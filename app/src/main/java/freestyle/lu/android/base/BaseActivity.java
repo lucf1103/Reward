d5e0c4301c9a794c7911fe6451ebd704cf5c3f7d
卢超凡 <lucf1103@163.com>
2018-09-17 17:54:06 +0800

    first

---
+++ app/src/main/java/freestyle/lu/android/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package freestyle.lu.android.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lu on 2018/9/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/freestyle/lu/android/review/MainActivity.java
@@ -0,0 +1,17 @@
+package freestyle.lu.android.review;
+
+import android.os.Bundle;
+
+import freestyle.lu.android.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
